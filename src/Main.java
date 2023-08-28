43460c41117d323a402299edd1868264bb2652c0
Grigoryower <140705935+Grigoryower@users.noreply.github.com>
2023-08-28 07:15:08 +0300

    Work0.2

--- src/Main.java
+++ src/Main.java
@@ -40,35 +40,34 @@ public class Main {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        for (int i = number.length - 1; i > -1; i--) {
-            System.out.print(number[i] + ", ");
+        for (int i = number.length - 1; i >= 0; i--) {
+            System.out.print(number[i]);
+            if (i > 0) {
+
+                System.out.print(", ");
+            }
         }
+
         System.out.println();
 
         double[] fractionalNumber = {1.57, 7.654, 9.986};
-        for (int i = fractionalNumber.length - 1; i > -1; i--) {
-            if (i == fractionalNumber.length - 1) {
-                System.out.print(fractionalNumber[i] + ",");
-            } else {
-                System.out.print(fractionalNumber[i] + " ");
+        for (int i = fractionalNumber.length - 1; i >= 0; i--) {
+            System.out.print(fractionalNumber[i]);
+            if (i > 0) {
+                System.out.print(", ");
             }
-
-
         }
         System.out.println();
-
         int[] integerArray = {24, 48, 72, 96, 120};
-        for (int i = integerArray.length  -1; i > -1; i--) {
-            if (i == integerArray.length - 1) {
-                System.out.print(integerArray[i] + ",");
-            } else {
-                System.out.print(integerArray[i] + " ");
+        for (int i = integerArray.length - 1; i >= 0; i--) {
+            System.out.print(integerArray[i]);
+            if (i > 0) {
+                System.out.print(", ");
             }
         }
         System.out.println();
-// неполучается вывести запятые как сказанно в условии
-
     }
+
 
     public static void task4() {
         System.out.println("Задача 4");
@@ -76,18 +75,15 @@ public class Main {
         number[0] = 7;
         number[1] = 3;
         number[2] = 1;
-        for (int i = 0; i <number.length; i++)
+        for (int i = 0; i < number.length; i++)
+            if (number[i] % 2 != 0) {
+                number[i]++;
 
-
-            System.out.println(number[i] + 1);
-
-
-
-
+                System.out.println(number[i]);
+            }
 
 
     }
-
 
 
 }
